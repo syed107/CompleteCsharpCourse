4e44a6774b8a92775eb9ad7c9bbb36c46ce85052
Syed107 <syedmuzammilahmed107@gmail.com>
2025-07-12 18:18:00 +0530

    new files

---
+++ Basic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpbasicLearning
+{
+    class Basic
+    {
+        public int CustId;
+        public string CustName;
+        public string CustAddress;
+        public Basic(int id, string name, string address)
+        {
+            CustId = id;
+            CustName = name;
+            CustAddress = address;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Customer ID: " + CustId);
+            Console.WriteLine("Customer Name: " + CustName);
+            Console.WriteLine("Customer Address: " + CustAddress);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpbasicLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Basic basic= new Basic(1, "John Doe", "123 Main St");
+            basic.Display();
+        }
+    }
+}
